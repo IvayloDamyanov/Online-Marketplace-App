069be78dfc1fb991b39d9ecd8a32e0edbfd37196
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-07-17 21:01:18 +0300

    Added passport configuration.

--- config/config.js
+++ config/config.js
@@ -2,5 +2,6 @@
 
 const port = 3001;
 const connectionString = 'mongodb://localhost/advertisment-db';
+const sessionSecret = 'Very Secret';
 
-module.exports = { port, connectionString };
+module.exports = { port, connectionString, sessionSecret };
