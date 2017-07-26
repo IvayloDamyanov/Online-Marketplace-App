ecab78f9b0e7af56a816004f5b48c4accb413b39
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-07-26 22:25:05 +0300

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+
+gulp.task('server:start', () => {
+    return require('./server');
+});
+
+const config = {
+    connectionString: 'mongodb://localhost/advertisment-db',
+    port: 3001,
+};
+
+gulp.task('start:server', () => {
+    return Promise.resolve()
+        .then(() => require('./db').init(config.connectionString))
+        .then((db) => require('./data').init(db))
+        .then((data) => require('./app').init(data))
+        .then((app) => {
+            app.listen(
+                config.port,
+                () => console.log(`Magic happens at :${config.port}`));
+        });
+});
+
+const { MongoClient } = require('mongodb');
+
+gulp.task('server:stop', () => {
+    return MongoClient.connect(config.connectionString)
+        .then((db) => {
+            return db.dropDatabase();
+        });
+});
