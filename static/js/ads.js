aa251775e069f63efa3bd4467161985045977897
IvayloDamyanov <ivaylo.damyanov@gmail.com>
2017-07-29 01:51:41 +0300

    Favourites/Delete ad/smaller fixes

--- static/js/ads.js
+++ static/js/ads.js
@@ -1,5 +1,5 @@
-var el = document.querySelector('#delete-ad');
-el.addEventListener('click', function (e) {
+var del = document.querySelector('#delete-ad');
+del.addEventListener('click', function (e) {
       const num = $('#delete-ad').data('adnum');
       $.ajax({
             url: '/adverts/ads/',
@@ -8,5 +8,18 @@ el.addEventListener('click', function (e) {
       })
       .then((response) => {
             location = response.redirect('search');
+      });
+});
+
+var fav = document.querySelector('#fav-ad');
+fav.addEventListener('click', function (e) {
+      const num = $('#fav-ad').data('adnum');
+      $.ajax({
+            url: '/adverts/favs/',
+            type: 'POST',
+            data: {num: num},
+      })
+      .then((response) => {
+            location = response.redirect('');
       });
 });
