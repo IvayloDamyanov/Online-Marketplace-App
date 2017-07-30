efcf38b9a1437d703ac3ca2df2e7fb5445370e81
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-07-30 22:33:58 +0300

    Added user.model validation.

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,13 @@
 class User {
     static isValid(model) {
-        return true;
+        if (typeof model !== 'undefined' &&
+           typeof model.username === 'string' &&
+           typeof model.password === 'string' &&
+           model.username.match(/^\w{3,20}$/g) &&
+           model.password.match(/^\w{4,20}$/g)) {
+               return true;
+        }
+        return false;
     }
 
     get id() {
